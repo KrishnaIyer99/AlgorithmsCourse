f9207782480669df29db0349b4ec7f6318930197
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-09-15 16:58:33 -0400

    Construct 2d array from text file

--- DijkstrasAlgo/DijkstraAlgo.py
+++ DijkstrasAlgo/DijkstraAlgo.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+"""CMPE 365 - Lab 1 (not for grades) Impletment dijkstras algorithm"""
 
 with open("C:\\Users\\Krishna Iyer\\Desktop\\Dijkstra_Data_6.txt") as textFile:
     lines = [line.split() for line in textFile]
 
 arraySize = int(lines[0][0])
 print(arraySize)
+
+#construct 2d Array
+new_arr = []
+for i in range(arraySize+1):
+    if(lines[i] == lines[0]):
+        continue
+    else:
+        row = []
+        for j in range(arraySize):
+            row.append(int(lines[i][j]))
+        new_arr.append(row)
+print(new_arr)
