cb38634b97b99982d5466b0a7b243d1dc76b00c4
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-09-19 15:07:07 -0400

    Finds the shortest calculated weighted path
    From vertex 0 to the farthest point in graph (real dijkstra algo)

--- DijkstrasAlgo/DijkstraAlgo.py
+++ DijkstrasAlgo/DijkstraAlgo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-"""CMPE 365 - Lab 1 (not for grades) Impletment dijkstras algorithm"""
-
-with open("Dijkstra_Data_6.txt") as textFile:
+"""CMPE 365 - Lab 1 (not for grades) Implement dijkstras algorithm"""
+#read in text file
+with open("Dijkstra_Data_1600.txt") as textFile:
     lines = [line.split() for line in textFile]
 
 arraySize = int(lines[0][0])
@@ -17,13 +17,48 @@ for i in range(arraySize+1):
         for j in range(arraySize):
             row.append(int(lines[i][j]))
         new_arr.append(row)
-d = [0]*arraySize
-vertex = 0
-for i in range(arraySize):
-    counter = 0
-    for j in new_arr:
-        d[counter] = j[vertex]
-        counter = counter + 1
-    print("Vertex {} furthest from vertex {} - Cost: {}\n".format(str(vertex), str(d.index(max(d))), str(max(d))))
-    vertex = vertex + 1
+
+#algorithm
+def dijkstra(arr):
+    #initialize arrays
+    A = 0
+    W = arr
+    cost = [A]*arraySize
+    predecessor = [A]*arraySize
+    reached = [False]*arraySize
+    reached[A] = True
+    estimate = [99999]*arraySize
+    candidate = [False]*arraySize
+    for k in range(arraySize):
+        if(W[A][k] > 0):
+            estimate[k] = W[A][k]
+            candidate[k] = True
+    predecessor[A] = -1
+    num_reach = 1
+    most_distant = A
+    while(arraySize != num_reach):
+        best_candidate = 99999
+        v = 1
+        for x in range(arraySize):
+            if(candidate[x] and estimate[x] < best_candidate):
+                v = x
+                best_candidate = estimate[x]
+        if(best_candidate == 99999):
+            break
+        cost[v] = estimate[v]
+        reached[v] = True
+        candidate[v] = False
+        if(cost[v] > cost[most_distant]):
+            most_distant = v
+        for z in range(arraySize):
+            if(W[v][z] > 0 and reached[z] == False):
+                candidate[z] = True
+                if(cost[v] + W[v][z] < estimate[z]):
+                    estimate[z] = cost[v] + W[v][z]
+                    predecessor[z] = v
+        num_reach = num_reach + 1
+    if(arraySize != num_reach):
+        print("Not all points were reachable")
+    print("The vertex furthest from vertex {} is vertex {}, which has a cost of {}".format(str(A), str(most_distant), str(cost[most_distant])))
+                
         
