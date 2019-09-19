b1e858151b7c336976470f8942d2c25bf8126d0d
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-09-19 12:40:33 -0400

    completed algorithm (added more test cases)

--- DijkstrasAlgo/DijkstraAlgo.py
+++ DijkstrasAlgo/DijkstraAlgo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """CMPE 365 - Lab 1 (not for grades) Impletment dijkstras algorithm"""
 
-with open("C:\\Users\\Krishna Iyer\\Desktop\\Dijkstra_Data_6.txt") as textFile:
+with open("Dijkstra_Data_6.txt") as textFile:
     lines = [line.split() for line in textFile]
 
 arraySize = int(lines[0][0])
@@ -17,4 +17,13 @@ for i in range(arraySize+1):
         for j in range(arraySize):
             row.append(int(lines[i][j]))
         new_arr.append(row)
-print(new_arr)
+d = [0]*arraySize
+vertex = 0
+for i in range(arraySize):
+    counter = 0
+    for j in new_arr:
+        d[counter] = j[vertex]
+        counter = counter + 1
+    print("Vertex {} furthest from vertex {} - Cost: {}\n".format(str(vertex), str(d.index(max(d))), str(max(d))))
+    vertex = vertex + 1
+        
