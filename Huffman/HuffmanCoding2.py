a118654d62cc0de79921b9444e2d245d1e9aa44b
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-11-04 17:56:27 -0500

    Completed part 1 and part 2

--- Huffman/HuffmanCoding2.py
+++ Huffman/HuffmanCoding2.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov  1 19:36:19 2019
+Created on Mon Nov  4 17:05:44 2019
 
-@author: krish
+@author: Krishna Iyer
 """
-
 from collections import Counter
 from heapq import heappush, heappop, heapify
+import os
 
-def code_building():
+def code_building(input_f, output_f):
     #get character frequency from text file
-    with open("EarthASCII.txt") as f:
+    with open(input_f) as f:
         c = Counter(f.read().strip())
-    f = c.most_common()#order characters from high freq to low
+        f = c.most_common()#order characters from high freq to low
     
     ftree = {}
     ftree[chr(10)] = 0
@@ -33,43 +33,43 @@ def code_building():
         #hi[2] = "1"+hi[2]
         heappush(freq_lst, [low_br[0]+high_br[0]]+low_br[1:]+high_br[1:])#merge nodes and push to heap
     codes_dict = dict(sorted(heappop(freq_lst)[1:], key=lambda k: (len(k[-1]), k)))
-    file = open("CodeBuilder.txt", "w+")
+    file = open(output_f, "w+")
     file.write("ASCII -> CODE\n")
     for symbol, code in codes_dict.items():
         file.write("%d -> %s" % (ord(symbol), code) +"\n")
     file.close()
     return codes_dict
 
-def encoder(converter):
+def encoder(converter, output_f):
     file_name = input("Enter file name: ")
     f = open(file_name)
     content = f.read()
-    f_encoded = open("EncodedFile.txt", "w+")
+    f_encoded = open(output_f, "w+")
     for c in content:
         f_encoded.write(converter[c])
     f_encoded.close()
     f.close()
 
-def decoder(converter):
+def decoder(converter, decoded_f):
     file_name = input("Enter name of encoded file name: ")
     f = open(file_name)
     digits = list(f.read())
     f.close()
-    f_decoded = open("decoderFile.txt", "w+")
+    f_decoded = open(decoded_f, "w+")
     start = 0
     end = 0
     window = []
     decoder = dict([(v,k) for k, v in converter.items()])
 
     while end<len(digits):
-        myString = ""
-        myString = myString.join(window)
+        bits = ""
+        bits = bits.join(window)
         if start == end:
             window = [digits[start], digits[end+1]]
             end+= 1
 
-        if myString in decoder.keys():
-            f_decoded.write(decoder[myString])
+        if bits in decoder.keys():
+            f_decoded.write(decoder[bits])
             #print(myString)
             #print(decoder[myString]+"\n")
             end+=1
@@ -80,56 +80,66 @@ def decoder(converter):
             end+=1
             window.extend(digits[end])
     f_decoded.close()
-    
-    
-    
-    
-    
-    
-    
+
+
+#Combine all data files to be converted
+with open("DATA.txt", 'w') as outfile:
+    for f in os.listdir("Data 20191031"):
+        file = open("Data 20191031"+'/'+f)
+        content = file.read()
+        outfile.write(content)
+        file.close()
+    outfile.close()
+
+
+#Canonical Collection 1
+
+codes = code_building("Canonical Collection 1 20191031\\"+os.listdir("Canonical Collection 1 20191031")[0], "CC1Codes.txt")
+encoder(codes, "DATA-CC1.txt")
+
+#Canonical Collection 2
+
+#Combine all files to get total frequency
+with open("CC2.txt", 'w') as outfile:
+    for f in os.listdir("Canonical Collection 2 20191031"):
+        file = open("Canonical Collection 2 20191031"+'/'+f)
+        content = file.read()
+        outfile.write(content)
+        file.close()
+    outfile.close()
+
+codes = code_building("CC2.txt", "CC2Codes.txt")
+encoder(codes, "DATA-CC2.txt")
+
+#Canonical Collection 3
+#Combine all files to get total frequency
+with open("CC3.txt", 'w') as outfile:
+    for f in os.listdir("Canonical Collection 3 20191031"):
+        file = open("Canonical Collection 3 20191031"+'/'+f)
+        content = file.read()
+        outfile.write(content)
+        file.close()
+    outfile.close()
+
+codes = code_building("CC3.txt", "CC3Codes.txt")
+encoder(codes, "DATA-CC3.txt")
 
 
 
 
 
-    
-"""
-    
-    
-    
 
 
 
-    
 
 
-frequencyTree = {}
-frequencyTree[chr(10)] = 0
-for l in range(32, 127):
-   frequencyTree[chr(l)] = 0
-with open("EarthASCII.txt") as f:
-    content = f.read()
 
-for char in content:
-    frequencyTree[char] += 1
 
-heap = [[wt, [sym, ""]] for sym, wt in frequencyTree.items()]
 
-heapify(heap)
-while len(heap) > 1:
-    lo = heappop(heap)
-    hi = heappop(heap)
-    for pair in lo[1:]:
-        pair[1] = '0' + pair[1]
-        #print("lo")
-        #print(pair)
-    for pair in hi[1:]:
-        pair[1] = '1' + pair[1]
-        #print(hi)
-        #print(pair)
-    heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-    #print(heap)
-huff = sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
 
-    
-"""     
+
+
+
+
+
+
